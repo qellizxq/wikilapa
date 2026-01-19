0fe3cfc87b8086b0383838a9d577815189807686
qellizxq <elizabqsxc@gmail.com>
2026-01-19 19:55:35 +0200

    Add files via upload
    Add user interaction using JS events

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("JS pieslēgts un darbojas");
+console.log("JS pieslēgts un darbojas");
+
+// elements
+const button = document.getElementById("changeBtn");
+const input = document.getElementById("nameInput");
+const output = document.getElementById("outputText");
+
+// CLICK notikums
+button.addEventListener("click", () => {
+  output.textContent = "Sveiki, " + input.value + "!";
+  output.style.color = "blue";
+});
+
+// INPUT notikums
+input.addEventListener("input", () => {
+  output.textContent = "Tu raksti: " + input.value;
